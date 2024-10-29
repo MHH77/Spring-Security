9490deb623266bb52bda3e5fe769082d349eb357
MHH77 <mesfandiari77@gmail.com>
2024-10-29 11:30:01 +0330

    [feature] username and password in config file

--- src/main/java/org/mhh/Config/SecurityConfig.java
+++ src/main/java/org/mhh/Config/SecurityConfig.java
@@ -6,23 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public InMemoryUserDetailsManager user() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("mohammad")
-                        .password("{noop}myjava123")
-                        .authorities("read")
-                        .build()
-        );
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
