1d46e97fa58e3bbf1f76161fc9b79d3f42da2a49
MHH77 <mesfandiari77@gmail.com>
2024-11-11 18:41:19 +0330

    [feature] Add CORS Config

--- src/main/java/org/mhh/Config/ProjectSecurityConfig.java
+++ src/main/java/org/mhh/Config/ProjectSecurityConfig.java
@@ -1,11 +1,16 @@
 package org.mhh.Config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -13,9 +18,22 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         //spring security by default block all the post and put and other http method that change data in backend or database by csrf
-        http.csrf().disable()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        //other domain address that want to use my app
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowCredentials(true);
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfiguration.setMaxAge(3600L);
+                        return corsConfiguration;
+                    }
+                }).and()
+                .csrf().disable()
                 .authorizeHttpRequests().requestMatchers("/myAccount", "/myLoans", "/myCards", "/myBalance").authenticated()
-                .requestMatchers("/contract", "/notices","/user/register","/actuator/**").permitAll()
+                .requestMatchers("/contract", "/notices", "/user/register", "/actuator/**").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
