ef2bd8afd20f8ddb0da3c42d9abdb93113ec6c1f
MHH77 <mesfandiari77@gmail.com>
2024-11-16 05:30:54 -0800

    [feature] Enable debug mode of spring security (not recommended in production environment)

--- src/main/java/org/mhh/App.java
+++ src/main/java/org/mhh/App.java
@@ -2,8 +2,10 @@ package org.mhh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true) // do not recommend for production environment
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
