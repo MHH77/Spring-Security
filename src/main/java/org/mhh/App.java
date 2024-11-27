cbb58676be95d6a36c5d846b0b96e679577a192f
MHH77 <mesfandiari77@gmail.com>
2024-11-27 11:08:42 +0330

    [enhancement] Disable spring security debug mode

--- src/main/java/org/mhh/App.java
+++ src/main/java/org/mhh/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true) // do not recommend for production environment
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
