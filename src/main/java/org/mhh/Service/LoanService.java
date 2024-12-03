1e025587d00e07c9dc79ccf2cf0aaef06cea2197
MHH77 <mesfandiari77@gmail.com>
2024-12-03 07:19:18 +0330

    [feature] Implement PostAuthorize on loans service

--- src/main/java/org/mhh/Service/LoanService.java
+++ src/main/java/org/mhh/Service/LoanService.java
@@ -1,18 +1,21 @@
 package org.mhh.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mhh.Repository.LoanRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LoanService {
 
     private final LoanRepository loanRepository;
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PostAuthorize("hasRole('ADMIN')")
     public String getLoanDetails() {
+        log.info("Get loan details");
         return "Here are the loans detail from the DB";
     }
 
